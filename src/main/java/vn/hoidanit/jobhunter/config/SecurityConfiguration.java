6090dbfa9bd4dcaa2ad2e72b77d20e8ea16b29db
demohcmus <lqngo123@gmail.com>
2024-09-23 12:37:00 +0700

    create token for jwt

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package vn.hoidanit.jobhunter.config;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,15 +13,24 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
+
+import vn.hoidanit.jobhunter.util.SecurityUtil;
+
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 
 public class SecurityConfiguration {
+    @Value("${hoidanit.jwt.base64-secret}")
+    private String jwtKey;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -36,6 +49,17 @@ public class SecurityConfiguration {
                 .formLogin(f -> f.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.from(jwtKey).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length,
+                SecurityUtil.JWT_ALGORITHM.getName());
     }
 
 }
