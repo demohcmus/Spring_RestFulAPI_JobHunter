6122f730b632fb0160bd97228d780823a9438df9
demohcmus <lqngo123@gmail.com>
2024-10-01 19:01:05 +0700

    update

--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -3,11 +3,9 @@ package vn.hoidanit.jobhunter.domain;
 import java.time.Instant;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -48,7 +46,7 @@ public class Company {
 
     private String updatedBy;
 
-    @OneToMany(mappedBy = "company")
+    @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
     private List<User> users;
 
     @PrePersist
