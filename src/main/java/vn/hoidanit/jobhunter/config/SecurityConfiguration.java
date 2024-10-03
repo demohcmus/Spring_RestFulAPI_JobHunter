38b58d4a0d4af3ebf040e1117d34bacb97c47bb6
demohcmus <lqngo123@gmail.com>
2024-10-03 12:27:13 +0700

    review fetch All Resume

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -49,22 +49,23 @@ public class SecurityConfiguration {
             HttpSecurity http,
             CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
 
-                String[] whiteList = {
-                    "/",
-                    "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
-                     "/storage/**",
-                    "/api/v1/companies/**", "/api/v1/jobs/**"
-            };
-    
+        String[] whiteList = {
+                "/",
+                "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
+                "/storage/**",
+                "/api/v1/companies/**", "/api/v1/jobs/**"
+        };
 
-            
         http
                 .csrf(c -> c.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         authz -> authz
-                                .requestMatchers(whiteList)
-                                .permitAll()
+                                .requestMatchers(whiteList).permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/companies").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/jobs").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/skills").permitAll()
+
                                 .anyRequest().authenticated())
                 // .anyRequest().permitAll())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
